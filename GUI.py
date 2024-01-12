425c51dfee1b17b68be03c660b989f948e5227d4
Matthew Svenson <matthewjsvenson@gmail.com>
2024-01-11 22:40:57 -0500

    Update GUI.py
    updates to tic tac toe GUI:
    Completed the gameboard (no functionality implemented yet)
    Completed the text for the game (and created the variable for it to alternate)

--- GUI.py
+++ GUI.py
@@ -15,13 +15,35 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            pygame.quit();
+
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
     # RENDER YOUR GAME HERE
     pygame.display.set_caption("EVIL Tic-Tac-Toe")
     # flip() the display to put your work on screen
+    pygame.draw.rect(screen, (0,0,0), pygame.Rect(60, 80, 600, 600), 2)
+    # Vertical lines for the tic tac toe game
+    pygame.draw.line(screen, (0,0,0), (260,80), (260,678))
+    pygame.draw.line(screen, (0,0,0), (460,80), (460,678))
+    # Horizontal lines for the tic tac toe game
+    pygame.draw.line(screen, (0,0,0), (60,280), (659,280))
+    pygame.draw.line(screen, (0,0,0), (60,480), (659,480))
+
+    # Text for the game
+    turn = "O"
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    text = font.render('It is {}\'s turn'.format(turn), True, (0,0,0), (255,255,255))
+    textRect = text.get_rect()
+    textRect.center = (360, 20)
+    screen.blit(text, textRect)
+
+
+
     pygame.display.flip()
+
 
     clock.tick(60)  # limits FPS to 60
 
