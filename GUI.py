c91968465c3dbc61de5ed265094a357a8ee5d97d
Matthew Svenson <matthewjsvenson@gmail.com>
2024-01-13 21:39:30 -0500

    Update GUI.py
    lots of work done on the frontend:
    First box has full functionality, now I just need to copy this functionality to the rest of the boxes and that should be the frontend done

--- GUI.py
+++ GUI.py
@@ -2,24 +2,74 @@
 import pygame
 
 
+#def drawTurn(xStart, yStart, xEnd, yEnd, turn):
+#    if turn == "X":
+#        pygame.draw.line(screen, (0,0,0), (xStart, yStart), (xEnd, yEnd))
+#        pygame.draw.line(screen, (0,0,0), (xEnd, yStart), (xStart, yEnd))
+
+boxs = [0,0,0,0,0,0,0,0,0]
+turn = 'X'
+
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((720, 720))
 clock = pygame.time.Clock()
 running = True
 
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
+    
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("white")
+    
+   
+    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # Provides functionality for pressing on the box
         if event.type == pygame.MOUSEBUTTONDOWN: 
-            pygame.quit();
+            # Row 1 functionality
+            if boxs[0] == 0:
+                if 63 <= mouse[0] <= 258 and 85 <= mouse[1] <= 277: 
+                # Edit the box to turn into right symbol
+                    if boxs[0] == 0:
+                        if turn == "X":
+                            boxs[0] = 1
+                        if turn == "O":
+                            boxs[0] = 2
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
+                    if turn == "X":
+                        turn = "O"
+                    else:
+                        if turn == "O":
+                            turn = "X"
+
+            if 263 <= mouse[0] <= 458 and 85 <= mouse[1] <= 277: 
+                pygame.quit();
+            if 463 <= mouse[0] <= 658 and 85 <= mouse[1] <= 277: 
+                pygame.quit();
+
+            # Row 2 functionality
+            if 63 <= mouse[0] <= 258 and 285 <= mouse[1] <= 477: 
+                pygame.quit();
+            if 263 <= mouse[0] <= 458 and 285 <= mouse[1] <= 477: 
+                pygame.quit();
+            if 463 <= mouse[0] <= 658 and 285 <= mouse[1] <= 477: 
+                pygame.quit();
+            
+            # Row 3 functionality
+            if 63 <= mouse[0] <= 258 and 485 <= mouse[1] <= 677: 
+                pygame.quit();
+            if 263 <= mouse[0] <= 458 and 485 <= mouse[1] <= 677: 
+                pygame.quit();
+            if 463 <= mouse[0] <= 658 and 485 <= mouse[1] <= 677: 
+                pygame.quit();
 
     # RENDER YOUR GAME HERE
     pygame.display.set_caption("EVIL Tic-Tac-Toe")
@@ -33,14 +83,21 @@ while running:
     pygame.draw.line(screen, (0,0,0), (60,480), (659,480))
 
     # Text for the game
-    turn = "O"
     font = pygame.font.Font('freesansbold.ttf', 32)
     text = font.render('It is {}\'s turn'.format(turn), True, (0,0,0), (255,255,255))
     textRect = text.get_rect()
     textRect.center = (360, 20)
     screen.blit(text, textRect)
 
+    # Get the mouse position for checking which button was pressed
+    mouse = pygame.mouse.get_pos() 
 
+    print(turn)
+
+    # Draws current status of board
+    if boxs[0] == 1:
+         pygame.draw.line(screen, (0,0,0), (63, 85), (255, 277))
+         pygame.draw.line(screen, (0,0,0), (255, 85), (63, 277))
 
     pygame.display.flip()
 
